90796c4089fa188557959a087b34327d2b7b5746
woogym <kwj9294@gmail.com>
2024-04-24 02:55:32 +0900

    Hello.java 수정

--- a202014003_test5/src/Hello.java
+++ a202014003_test5/src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("one");
+        System.out.println("수정했습니다");
     }
 }
